06371a5bed79a94f7a1f6f3372585b23b65732f2
Silva Vähämaa <silva.maa44@gmail.com>
2024-03-05 18:08:05 +0200

    new file:   .DS_Store 	modified:   vite-project/start-api-harjoituspohja.js

--- vite-project/start-api-harjoituspohja.js
+++ vite-project/start-api-harjoituspohja.js
@@ -1,10 +1,10 @@
-import "./style.css";
-import { fetchData } from "./fetch.js";
+import './style.css';
+import { fetchData } from './fetch.js';
 
-const bt1 = document.querySelector(".get_entry");
-bt1.addEventListener("click", async () => {
-  console.log("Klikki toimii");
-  const url = "http://localhost:3001/api/entries/1";
+const bt1 = document.querySelector('.get_entry');
+bt1.addEventListener('click', async () => {
+  console.log('Klikki toimii');
+  const url = 'http://localhost:3001/api/entries/1';
 
   fetchData(url).then((data) => {
     // käsitellään fetchData funktiosta tullut JSON
@@ -15,30 +15,42 @@ bt1.addEventListener("click", async () => {
   // # GET http://localhost:3000/api/entries/:id
 });
 
-const allButton = document.querySelector(".get_users");
-allButton.addEventListener("click", getUsers);
+// Haetaan kaikki käyttäjät ja luodaan niistä taulukko
+// 1. Hae ensin nappula ja kutsu funktiota (keksi nimi)
+
+const allButton = document.querySelector('.get_users');
+allButton.addEventListener('click', getUsers);
 
 async function getUsers() {
-  console.log("Täällä ollaan!");
-  const url = "http://localhost:3001/api/users";
-  let token = localStorage.getItem("token");
+  console.log('Haetaa kaikki käyttäjät');
+  const url = 'http://127.0.0.1:3001/api/users';
+  let token = localStorage.getItem('token');
   const options = {
-    method: "GET",
+    method: 'GET',
     headers: {
-      Authorization: "Bearer: " + token,
+      Authorization: 'Bearer: ' + token,
     },
   };
 
   fetchData(url, options).then((data) => {
     createTable(data);
   });
+
+  // vaihtoehtoinen tapa
+  // try {
+  //   const responseData = await fetchData(url, options);
+  //   console.log(responseData);
+  // } catch (error) {
+  //   console.error(error);
+  // }
 }
 
 function createTable(data) {
   console.log(data);
 
   // etitään tbody elementti
-  const tbody = document.querySelector(".tbody");
+  const tbody = document.querySelector('.tbody');
+  tbody.innerHTML = '';
 
   // loopissa luodaan jokaiselle tietoriville oikeat elementit
   // elementtien sisään pistetään oikeat tiedot
@@ -47,42 +59,42 @@ function createTable(data) {
     console.log(rivi.user_id, rivi.username, rivi.user_level);
 
     // Luodaan jokaiselle riville ensin TR elementti alkuun
-    const tr = document.createElement("tr");
+    const tr = document.createElement('tr');
 
     // Luodaan soluja mihihin tiedot
-    const td1 = document.createElement("td");
+    const td1 = document.createElement('td');
     td1.innerText = rivi.username;
 
     // Luodaan soluja mihihin tiedot
-    const td2 = document.createElement("td");
+    const td2 = document.createElement('td');
     td2.innerText = rivi.user_level;
 
     // <td><button class="check" data-id="2">Info</button></td>
     // const td3 = document.createElement('td');
     //td3.innerHTML = `<button class="check" data-id="${rivi.user_id}">Info</button>`;
 
-    const td3 = document.createElement("td");
-    const button1 = document.createElement("button");
-    button1.className = "check";
-    button1.setAttribute("data-id", rivi.user_id);
-    button1.innerText = "Info";
+    const td3 = document.createElement('td');
+    const button1 = document.createElement('button');
+    button1.className = 'check';
+    button1.setAttribute('data-id', rivi.user_id);
+    button1.innerText = 'Info';
     td3.appendChild(button1);
 
-    button1.addEventListener("click", getUser);
+    button1.addEventListener('click', getUser);
 
     // td4
-    const td4 = document.createElement("td");
-    const button2 = document.createElement("button");
-    button2.className = "del";
-    button2.setAttribute("data-id", rivi.user_id);
-    button2.innerText = "Delete";
+    const td4 = document.createElement('td');
+    const button2 = document.createElement('button');
+    button2.className = 'del';
+    button2.setAttribute('data-id', rivi.user_id);
+    button2.innerText = 'Delete';
     td4.appendChild(button2);
 
     // 2. Lisää kuuntelija kun taulukko on tehty
-    button2.addEventListener("click", deleteUser);
+    button2.addEventListener('click', deleteUser);
 
     // td5
-    var td5 = document.createElement("td");
+    var td5 = document.createElement('td');
     td5.innerText = rivi.user_id;
 
     tr.appendChild(td1);
@@ -95,15 +107,49 @@ function createTable(data) {
 }
 
 function getUser() {
-  console.log("Haet tietoa");
+  console.log('Haet tietoa');
 }
 
-function deleteUser() {
-  console.log("Deletoit tietoa");
+function deleteUser(evt) {
+  console.log('Deletoit tietoa');
+  console.log(evt);
+
+  // Tapa 1, haetaan arvo tutkimalla eventtiä
+  const id = evt.target.attributes['data-id'].value;
+  console.log(id);
+
+  // Tapa 2 haetaan "viereinen elementti"
+  const id2 = evt.target.parentElement.nextElementSibling.textContent;
+  console.log('Toinen tapa: ', id2);
+
+  // <tr>
+  //   <td>Melissa</td>
+  //   <td>5150</td>
+  //   <td><button class="check" data-id="2">Info</button></td>
+  //   <td><button class="del" data-id="2">Delete</button></td>
+  //   <td>2</td>
+  // </tr>
+
+  const url = `http://127.0.0.1:3001/api/users/${id}`;
+  let token = localStorage.getItem('token');
+  const options = {
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Bearer: ' + token,
+    },
+  };
+
+  const answer = confirm(`Oletko varma että haluat poistaa käyttäjän ID: ${id} `);
+  if (answer) {
+    fetchData(url, options).then((data) => {
+      console.log(data);
+      getUsers();
+    });
+  }
 }
 
 async function showUserName() {
-  console.log("Hei täällä ollaan! Nyt pitäisi hakea käyttäjän tiedot");
+  console.log('Hei täällä ollaan! Nyt pitäisi hakea käyttäjän tiedot');
 
   // hae käyttäjän omat tiedot
   // 1. joko lokal storagesta jos on tallessa
@@ -115,17 +161,17 @@ async function showUserName() {
 
   // 2. hae uudestaan /api/auth/me endpointin kautta
 
-  const url = "http://localhost:3001/api/auth/me";
-  let token = localStorage.getItem("token");
+  const url = 'http://localhost:3001/api/auth/me';
+  let token = localStorage.getItem('token');
   const options = {
-    method: "GET",
+    method: 'GET',
     headers: {
-      Authorization: "Bearer: " + token,
+      Authorization: 'Bearer: ' + token,
     },
   };
   fetchData(url, options).then((data) => {
     console.log(data);
-    document.getElementById("name").innerHTML = data.user.username;
+    document.getElementById('name').innerHTML = data.user.username;
     // muita hakuja ja tietoja sivulle, kuten email ym. mitä halutaan näyttää
   });
 }
@@ -139,10 +185,10 @@ showUserName();
 // sivuille on nyt myös lisätty navigaatio html sivuun, sekä siihen sopiva CSS koodi, hae siis uusi HTML ja UUSI CSS ennen kun aloitat
 
 async function getAllUsers() {
-  console.log("toimii!");
+  console.log('toimii!');
 
   try {
-    const response = await fetch("http://127.0.0.1:3001/api/users");
+    const response = await fetch('http://127.0.0.1:3001/api/users');
     console.log(response);
     const data = await response.json();
     console.log(data);
@@ -152,41 +198,41 @@ async function getAllUsers() {
     });
 
     // tänne tiedot
-    const tbody = document.querySelector(".tbody");
-    tbody.innerHTML = "";
+    const tbody = document.querySelector('.tbody');
+    tbody.innerHTML = '';
 
     data.forEach((element) => {
       console.log(element.username);
 
       // Create table row element
-      var tr = document.createElement("tr");
+      var tr = document.createElement('tr');
 
       // td1 Username
-      var td1 = document.createElement("td");
+      var td1 = document.createElement('td');
       td1.innerText = element.username;
 
       // td2
-      var td2 = document.createElement("td");
+      var td2 = document.createElement('td');
       td2.innerText = element.user_level;
 
       // td3
-      var td3 = document.createElement("td");
-      var button1 = document.createElement("button");
-      button1.className = "check";
-      button1.setAttribute("data-id", "1");
-      button1.innerText = "Info";
+      var td3 = document.createElement('td');
+      var button1 = document.createElement('button');
+      button1.className = 'check';
+      button1.setAttribute('data-id', '1');
+      button1.innerText = 'Info';
       td3.appendChild(button1);
 
       // td4
-      var td4 = document.createElement("td");
-      var button2 = document.createElement("button");
-      button2.className = "del";
-      button2.setAttribute("data-id", "1");
-      button2.innerText = "Delete";
+      var td4 = document.createElement('td');
+      var button2 = document.createElement('button');
+      button2.className = 'del';
+      button2.setAttribute('data-id', '1');
+      button2.innerText = 'Delete';
       td4.appendChild(button2);
 
       // td5
-      var td5 = document.createElement("td");
+      var td5 = document.createElement('td');
       td5.innerText = element.user_id;
 
       // Append table data elements to the table row element
